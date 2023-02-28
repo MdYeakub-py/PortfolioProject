d0f55709e9c9a491d5b2c34ec3e77e1c721dc3d0
Md. Yeakub <mdyeakub.cse@gmail.com>
2023-02-28 15:10:41 +0600

    new update

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -87,8 +87,13 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': "django.db.backends.postgresql_psycopg2",
+        'HOST': "db.nptaxumkeoydagwbaycf.supabase.co",
+        'NAME': "postgres",
+        'USER': "postgres",
+        'PASSWORD': "mdyeakub@123567",
+        'PORT': "5432",
+
     }
 }
 
